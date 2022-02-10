b80b530f96a1e5417756728e777ddb8797c55b41
IdaliaGpe <idaliaguadalupepucca@hotmail.com>
2022-02-10 16:28:02 -0700

    No salta

--- juego.py
+++ juego.py
@@ -1,29 +1,178 @@
-#Importar Librerias
-from turtle import width
+#Comandos para librerías
+#pip install pyopengl
+#pip install glfw
+
+#Importar librerias
+
+from sre_constants import JUMP
 from OpenGL.GL import *
 from glew_wish import *
 import glfw
+import math
+
+#Variables
+velocidad_x = 0.5
+velocidad_y = 0.5
+JUMP = False
+
+posicion_triangulo = [0.2,0.0,0.0]
+posicion_cuadrado = [-0.2, 0.0, 0.0]
+window = None
+
+tiempo_anterior = 0.0
 
 #Teclas
-#Tecla escape cierre ventana
 def key_callback(window, key, scancode, action, mods):
     #Que la tecla escape cierre ventana al ser presionado
     if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
-        glfw.set_window_should_close(window, 1)
+         glfw.set_window_should_close(window, 1)
+
+def actualizar():
+    global velocidad_x, velocidad_y
+    global tiempo_anterior
+    global window, JUMP
+    global posicion_triangulo
+    global posicion_cuadrado
+
+    tiempo_actual = glfw.get_time()
+    #Cuanto tiempo paso entre la ejecucion actual
+    #y la inmediata anterior de esta funcion
+    tiempo_delta = tiempo_actual - tiempo_anterior
+
+    #Leer los estados de las teclas que queremos
+    estado_tecla_arriba = glfw.get_key(window, glfw.KEY_UP)
+    estado_tecla_abajo = glfw.get_key(window, glfw.KEY_DOWN)
+    estado_tecla_derecha = glfw.get_key(window, glfw.KEY_RIGHT)
+    estado_tecla_izquierda = glfw.get_key(window, glfw.KEY_LEFT)
+
+    cantidad_movimiento = velocidad_x * tiempo_delta
+    if estado_tecla_arriba == glfw.PRESS:
+        posicion_triangulo[1] = posicion_triangulo[1] + cantidad_movimiento
+    if estado_tecla_derecha == glfw.PRESS:
+        posicion_triangulo[0] = posicion_triangulo[0] + cantidad_movimiento
+    if estado_tecla_abajo == glfw.PRESS:
+        posicion_triangulo[1] = posicion_triangulo[1] - cantidad_movimiento
+    if estado_tecla_izquierda == glfw.PRESS:
+        posicion_triangulo[0] = posicion_triangulo[0] - cantidad_movimiento
+
+
+    #Movimiento derecha / izquierda
+    estado_tecla_d = glfw.get_key(window, glfw.KEY_D)
+    estado_tecla_a = glfw.get_key(window, glfw.KEY_A)
+
+    if estado_tecla_d == glfw.PRESS:
+        posicion_cuadrado[0] = posicion_cuadrado[0] + cantidad_movimiento
+    if estado_tecla_a == glfw.PRESS:
+        posicion_cuadrado[0] = posicion_cuadrado[0] - cantidad_movimiento
+
+    if estado_tecla_d == glfw.PRESS:
+        posicion_cuadrado[0] = posicion_cuadrado[0] + cantidad_movimiento
+    if estado_tecla_a == glfw.PRESS:
+        posicion_cuadrado[0] = posicion_cuadrado[0] - cantidad_movimiento
+
+    #Salto
+    vel_y = velocidad_y * tiempo_delta
+
+    estado_tecla_space = glfw.get_key(window, glfw.KEY_SPACE)
+    if JUMP is False and estado_tecla_space == glfw.PRESS:
+        JUMP = True
+
+    if JUMP is True:
+        posicion_cuadrado[1] -= vel_y
+        vel_y -= 0.1
+        if vel_y < -0.5:
+            JUMP  = False
+            vel_y = 0.5
+
+    tiempo_anterior = tiempo_actual
+
+#Colision
+def colisoniando():
+    colisionando = False
+    #Extrema derecha del triangulo >= Extrema izquierda cuadrado
+    #Extrema izquierda del triangulo <= Extrema derecha cuadrado
+    #Extrema superior del triangulo >= Extrema inferior cuadrado
+    #Extrema inferior del triangulo <= Extrema superior cuadrado
+    if (posicion_triangulo[0] + 0.05 > posicion_cuadrado[0] - 0.05 
+    and posicion_triangulo[0] - 0.05 <= posicion_cuadrado[0] + 0.05 
+    and posicion_triangulo[1] + 0.05 > posicion_cuadrado[1] - 0.05 
+    and posicion_triangulo[1] - 0.05 <= posicion_cuadrado[1] + 0.05):
+        colisionando = True
+    return colisionando
+
+#Dibujar triangulo
+def draw_triangulo():
+    global posicion_triangulo
+    glPushMatrix()
+    glTranslatef(posicion_triangulo[0], posicion_triangulo[1],0.0)
+    glBegin(GL_TRIANGLES)
+
+    #Establecer color    
+    if colisoniando():
+        glColor3f(0,0,1)
+    else:
+        glColor3f(1,0,0)
+
+    #Manda vertices a dibujar
+    glVertex3f(-0.05,-0.05,0)
+    glVertex3f(0.0,0.05,0)
+    glVertex3f(0.05,-0.05,0)
+
+    glEnd()
+
+    glBegin(GL_LINE_LOOP)
+    glColor3f(0.0,0.0,0.0)
+    glVertex3f(-0.05,-0.05,0.0)
+    glVertex3f(-0.05,0.05,0.0)
+    glVertex3f(0.05,0.05,0.0)
+    glVertex3f(0.05,-0.05,0.0)
+
+    glEnd()
+
+    glPopMatrix()
+
+def draw_cuadrado():
+    global posicion_cuadrado
+    glPushMatrix()
+    glTranslatef(posicion_cuadrado[0], posicion_cuadrado[1], 0.0)
+    glBegin(GL_QUADS)
+    glColor3f(0.4, 0.9, 0.21)
+    glVertex3f(-0.05,0.05,0.0)
+    glVertex3f(0.05,0.05,0.0)
+    glVertex3f(0.05,-0.05,0.0)
+    glVertex3f(-0.05,-0.05,0.0)
+    glEnd()
+
+    glBegin(GL_LINE_LOOP)
+    glColor3f(0.0, 0.0, 0.0)
+    glVertex3f(-0.05,0.05,0.0)
+    glVertex3f(0.05,0.05,0.0)
+    glVertex3f(0.05,-0.05,0.0)
+    glVertex3f(-0.05,-0.05,0.0)
+    glEnd()
+
+    glPopMatrix()
+
+#Pintar
+def draw():
+
+    draw_cuadrado()
+    draw_triangulo()
 
 #Main
 def main():
-    width = 800
-    height = 800
+    global window
 
+    width = 700
+    height = 700
     #Inicializar GLFW
     if not glfw.init():
         return
 
-    #Declarar Ventana
-    window = glfw.create_window(width, height, "Mario", None, None)
+    #declarar ventana
+    window = glfw.create_window(width, height, "Mi ventana", None, None)
 
-    #Configuraciones de OPENGL
+    #Configuraciones de OpenGL
     glfw.window_hint(glfw.SAMPLES, 4)
     glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
     glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
@@ -41,34 +190,36 @@ def main():
     #Le dice a GLEW que si usaremos el GPU
     glewExperimental = True
 
-    #Inicializamos glew
+    #Inicializar glew
     if glewInit() != GLEW_OK:
         print("No se pudo inicializar GLEW")
         return
-    
-    #Imprimir Version
+
+    #imprimir version
     version = glGetString(GL_VERSION)
     print(version)
 
-    #Establecer el key callback
     glfw.set_key_callback(window, key_callback)
-
-    #Draw Loop
+    
+    #Draw loop
     while not glfw.window_should_close(window):
         #Establecer el viewport
-        glViewport(0, 0, width, height)
+        #glViewport(0,0,width,height)
         #Establecer color de borrado
-        glClearColor(162/255, 202/255, 223/255, 1)
-        #Borrar el contenido de viewport
+        glClearColor(0.7,0.7,0.7,1)
+        #Borrar el contenido del viewport
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        
-        #Dibujar
-        #Nos queda pendiente el dibujo
 
-        #Polling de Inputs
+
+        actualizar()
+        #Dibujar
+        draw()
+
+
+        #Polling de inputs
         glfw.poll_events()
 
-        #Canbia los buffers
+        #Cambia los buffers
         glfw.swap_buffers(window)
 
     glfw.destroy_window(window)
